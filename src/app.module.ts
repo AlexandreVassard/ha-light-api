6e390970913057878240dd2bbd795b032550ffa5
Alexandre Vassard <alexandre.vassard@bibliosansfrontieres.org>
2023-11-10 21:07:11 +0100

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HomeAssistantModule } from './home-assistant/home-assistant.module';
 import { SlackModule } from './slack/slack.module';
 
 @Module({
-  imports: [HomeAssistantModule, SlackModule],
+  imports: [SlackModule],
   controllers: [AppController],
   providers: [AppService],
 })
